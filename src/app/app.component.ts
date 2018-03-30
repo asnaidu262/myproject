25e57352d2051fc85ce55325967e54a1509c23d5
Adabala Srinivas Naidu <anaidu5@asiapac.globalcsc.net>
2018-03-30 14:20:50 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My first angulr app ';
+  isNewUserSelcted : boolean = false;
+  selectNewUser(){
+    this.isNewUserSelcted = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+import { UseraddComponent } from './useradd/useradd.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UseraddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
